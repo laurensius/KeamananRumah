d6b10d6a1cb6f3c47129f19e5f8360dea78833c7
Erwin Ligar Nugraha <33018434+RaveKun@users.noreply.github.com>
2017-11-09 18:08:38 +0700

    Perbaikan Minor

--- application/models/mod_device.php
+++ application/models/mod_device.php
@@ -6,14 +6,11 @@ class Mod_device extends CI_Model{
 		$this->db->insert('t_sensor',$data);
         return $this->db->affected_rows();
     }
-<<<<<<< HEAD
-=======
+
 
     function recent($API_KEY){
         $query_str = "Select * from t_sensor where API_KEY='".$API_KEY."' order by id desc limit 1";
         $result = $this->db->query($query_str);
         return $result->result();
     }
-
->>>>>>> caed26356b58de5dcfc5295674711d58f72f255b
 }
