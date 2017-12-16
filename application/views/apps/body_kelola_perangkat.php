10d6c2cd0dca12720006268aacffc59280e67a42
Laurensius Dede Suhardiman <burglar.tea@gmail.com>
2017-12-16 14:55:45 +0700

    Secure key

--- application/views/apps/body_kelola_perangkat.php
+++ application/views/apps/body_kelola_perangkat.php
@@ -32,8 +32,18 @@
                                         <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; min-height: 200px;">
                                             <div class="scroller" style="min-height: 200px; overflow: hide; width: auto;" data-initialized="1">
                                                 <center>
-                                                    <span id="btn_toggle"></span><br>
-                                                    <h3><span id="msg"></span></h3>
+                                                    <table>
+                                                    <tr>
+                                                    <td align="center"><h5><span id="msg"></span></h5></td>
+                                                    </tr>
+                                                    <tr>
+                                                    <td align="center"><input type="pasword" id="secure_key" name="secure_key" class="form-control" placeholder="Secure Key">
+                                                    </td>
+                                                    </tr>
+                                                    <tr>
+                                                    <td align="center"><br><span id="btn_toggle"></span><br></td>
+                                                    </tr>
+                                                    </table>
                                                 </center>    
                                             </div>
                                         </div>
@@ -69,6 +79,7 @@
                                 notif += 'Terjadi kesalahan pada saat load status prangkat. Silahkan coba lagi (refresh browser Anda).';
                                 notif += '</div>';
                                 document.getElementById('notif').innerHTML = notif;
+                                notif = '';
                                 // $("#notif").html(notif);
                             },
                         });
@@ -77,17 +88,28 @@
                 });
 
                 function changeState(state){
+                    var k = '<?php echo $this->session->userdata("session_appssystem_secure_key"); ?>';
                     if (confirm('Apakah Anda yakin akan mengubah status perangkat (end device) ?')) {
-                        $.ajax({
-                            url : '<?php echo site_url(); ?>/api/change_device_status/' + state + '/<?php echo $this->session->userdata("session_appssystem_api_key"); ?>/<?php echo $this->session->userdata("session_appssystem_id"); ?>/' ,
-                            type : 'GET',
-                            dataType : 'json',
-                            success : function(response){
-                                console.log(response);
-                            },
-                            error : function(response){
-                            },
-                        });
+                        if(k == $('#secure_key').val()){
+                            $.ajax({
+                                url : '<?php echo site_url(); ?>/api/change_device_status/' + state + '/<?php echo $this->session->userdata("session_appssystem_api_key"); ?>/<?php echo $this->session->userdata("session_appssystem_id"); ?>/' ,
+                                type : 'GET',
+                                dataType : 'json',
+                                success : function(response){
+                                    alert(response);
+                                },
+                                error : function(response){
+                                    
+                                },
+                            });
+                        }else{
+                            notif += '<div class="alert alert-danger alert-dismissable">';
+                            notif += 'Pastikan Anda sudah mengisi SECURE KEY dengan benar';
+                            notif += '</div>';
+                            document.getElementById('notif').innerHTML = notif;
+                            notif = '';
+                        }
+                        
                     }
                 }
                 </script>
